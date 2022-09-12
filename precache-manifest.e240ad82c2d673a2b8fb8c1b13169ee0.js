b590d67b92d58051654e20f04ac8c655890a178d
xiaopow <xiaoyang88@gmail.com>
2022-09-12 10:01:45 +0800

    Updates

---
+++ precache-manifest.e240ad82c2d673a2b8fb8c1b13169ee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62157af39c43c63f808b1723baec6e3",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "e576944e2c216f486dc8",
+    "url": "/first-react-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ae7fa6732bf2e5ead072",
+    "url": "/first-react-app/static/css/main.e23fee82.chunk.css"
+  },
+  {
+    "revision": "e576944e2c216f486dc8",
+    "url": "/first-react-app/static/js/2.eeaf7b38.chunk.js"
+  },
+  {
+    "revision": "ae7fa6732bf2e5ead072",
+    "url": "/first-react-app/static/js/main.d8b8838b.chunk.js"
+  },
+  {
+    "revision": "cb2d0d1ef7d3e2b845c4",
+    "url": "/first-react-app/static/js/runtime~main.f5c4d8dc.js"
+  }
+]);
